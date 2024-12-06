f4fc996b3ff3ff789cbe69a947f796ab518f80e8
UKIaw5 <shonen123aaa456@icloud.com>
2024-12-06 14:53:07 +0900

    END

--- init-mongo.js
+++ init-mongo.js
@@ -1,9 +1,19 @@
+db = db.getSiblingDB("admin");
+
+db.createUser({
+  user: "root",
+  pwd: "rootpassword",
+  roles: [
+    { role: "root", db: "admin" }
+  ]
+});
+
 db = db.getSiblingDB("chatapp");
+
 db.createUser({
   user: "chatappuser",
   pwd: "chatappuserpass",
   roles: [
-    { role: "readWrite", db: "chatapp" },
-    { role: "dbAdmin", db: "chatapp" },
-  ],
+    { role: "readWrite", db: "chatapp" }
+  ]
 });
